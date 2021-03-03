7e155de95128c665f5043b0606d44d81cab86180
Moritz Maxeiner <mm@ucw.sh>
2021-03-03 14:47:29 +0100

    [ci] snapshot 2021-03-03

--- ci/prepare.py
+++ ci/prepare.py
@@ -56,5 +56,5 @@ if __name__ == "__main__":
     for name, project, job in [
         ("biotracker-interfaces", "bioroboticslab/biotracker/interfaces", job_stem),
     ]:
-        with fetch_artifacts(project, "master", job) as artifacts:
+        with fetch_artifacts(project, "snapshot_20210303", job) as artifacts:
             extract_cmake_package(artifacts, name)
